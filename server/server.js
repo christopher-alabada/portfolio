88135eaa56247ddd8d5e77b8776bb30c5953e229
Christopher Alabada <christopher.alabada@gmail.com>
2019-07-09 10:42:30 +0900

    Added server files and gitignore

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const port = 5000;
+
+all.listen(port, () => console.log(`Server started on port ${port}`));
