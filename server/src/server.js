73f57b1d8a7166f2733135242734128535c301fc
Christopher Alabada <christopher.alabada@gmail.com>
2019-07-21 03:28:26 +0900

    Added csrf

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const https = require('https');
+// const https = require('https');
 const mongoose = require('mongoose');
-const session = require('express-session');
 const cors = require('cors');
 const fs = require('fs');
 const config = require('../config');
-
+const cookieParser = require('cookie-parser');
+const csurf = require('csurf');
 
 // set server port from config. Use 5000 if not set
 const port = config.server.port || 5000;
@@ -26,11 +26,14 @@ const app = express();
 
 // Server config
 app.use(express.json());
-app.use(cors({ origin: config.client.url }));
+app.use(cors({ credentials: true, origin: config.client.url }));
+app.use(cookieParser());
+app.use(csurf({ cookie: true }));
 
 // Routes
 app.use('/api/pages', require('./routes/api/pages'));
 app.use('/api/projects', require('./routes/api/projects'));
+app.use('/api/contacts', require('./routes/api/contacts'));
 app.use('/api/experiences', require('./routes/api/experiences'));
 
 // 404
@@ -43,14 +46,10 @@ app.use(function(err, req, res, next) {
   return res.status(500).json({ error: err });
 });
 
-// Start sessions
-const MongoStore = require('connect-mongo')(session);
-app.use(session({
-    store: new MongoStore({ mongooseConnection: mongoose.connection })
-}));
-
 // And finally, start server
-https.createServer({
-  key: fs.readFileSync(config.server.key),
-  cert: fs.readFileSync(config.server.crt)
-}, app).listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
+// https.createServer(app)
+// {
+//   key: fs.readFileSync(config.server.key),
+//   cert: fs.readFileSync(config.server.crt)
+// }
